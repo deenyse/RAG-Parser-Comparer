4ed173d204f4ec717f58f39dea4572f100f6c4ed
deenyse <denvery31@gmail.com>
2025-07-18 17:17:25 +0200

    created YAML file

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from Parsers.PdfPlumber import ParsByPdfPlumber
 from Tools.SymbolChunker import SymbolChunker
 
+import yaml
+
+
 if __name__ == "__main__":
-# TODO simplify parser structure, to remove redundant context manager construction
-#   move this construction into IChunker, so i can open Chunkers as a context(in the RAG pipline)
 
 
+    def load_config(file_path="config.yaml"):
+        try:
+            with open(file_path, "r") as file:
+                return yaml.safe_load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Config file {file_path} was not found.")
+        except yaml.YAMLError as e:
+            raise ValueError(f"YAML parsing issue: {str(e)}")
+
+
+    # Загружаем конфигурацию
+    config = load_config()
+    GEMINI_API_KEY = config.get("gemini_api", {}).get("key")
+
+    print("GEMINI API KEY: {}".format(GEMINI_API_KEY))
 
     # with SymbolChunker(ParsByPdfPlumber, "TestFiles/OverlapTest.pdf", 400, 70) as chunker:
     #     for block in chunker:
@@ -15,9 +31,9 @@ if __name__ == "__main__":
     chunker = SymbolChunker(ParsByPdfPlumber, "TestFiles/OverlapTest.pdf", 400, 70)
     chunker.open()
 
-    with SymbolChunker(ParsByPdfPlumber, "TestFiles/OverlapTest.pdf", 400, 70) as chunker:
-        for chunk in chunker:
-            print(chunk)
+    # with SymbolChunker(ParsByPdfPlumber, "TestFiles/OverlapTest.pdf", 400, 70) as chunker:
+    #     for chunk in chunker:
+    #         print(chunk)
 
     # #WORKS
     # parser = ParsByPdfPlumber()
